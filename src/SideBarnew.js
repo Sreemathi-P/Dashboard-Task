8e45ad8a264092592093fd72a27bb376304cbeb1
-u <-u>
2022-08-30 15:25:04 +0530

    login-page changes

--- src/SideBarnew.js
+++ src/SideBarnew.js
@@ -1,34 +1,34 @@
 import Nav from 'react-bootstrap/Nav';
 import { AiOutlineHome, AiOutlinePlus } from 'react-icons/ai';
-import { MdWorkOutline, MdBarChart } from 'react-icons/md'
-
+import { MdWorkOutline, MdBarChart } from 'react-icons/md';
+import Navbar from 'react-bootstrap/Navbar';
 
 function SideBarnew() {
   return (
-    <div className='sidebar-new'>
-      <Nav defaultActiveKey="/home" className="flex-column">
-      <br></br>
-      <Nav.Link href="/home" className='nav-icon'>
-        <AiOutlineHome className='home-icon' />
-        <h6 className='sideLabel'>Home</h6>
-      </Nav.Link>
+        <div className='sidebar-new'>
+          <Nav defaultActiveKey="/home" className="flex-column">
+            <br></br>
+            <Nav.Link href="/home" className='nav-icon'>
+              <AiOutlineHome className='home-icon' />
+              <h6 className='sideLabel'>Home</h6>
+            </Nav.Link>
 
-      <Nav.Link href="/work" className='nav-icon'>
-        <MdWorkOutline className='home-icon' />
-        <h6 className='sideLabel'>Work</h6>
-      </Nav.Link>
+            <Nav.Link href="/work" className='nav-icon'>
+              <MdWorkOutline className='home-icon' />
+              <h6 className='sideLabel'>Work</h6>
+            </Nav.Link>
 
-      <Nav.Link href="/reports" className='nav-icon'>
-        <MdBarChart className='home-icon' />
-        <h6 className='sideLabel'>Reports</h6>
-      </Nav.Link>
+            <Nav.Link href="/reports" className='nav-icon'>
+              <MdBarChart className='home-icon' />
+              <h6 className='sideLabel'>Reports</h6>
+            </Nav.Link>
 
-      <Nav.Link href="/create" className='nav-icon'>
-        <AiOutlinePlus className='home-icon' />
-        <h6 className='sideLabel'>Create</h6>
-      </Nav.Link>
-    </Nav>
-    </div>
+            <Nav.Link href="/create" className='nav-icon'>
+              <AiOutlinePlus className='home-icon' />
+              <h6 className='sideLabel'>Create</h6>
+            </Nav.Link>
+          </Nav>
+        </div>
   );
 }
 
